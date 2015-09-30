d15f108c0fd811282dc71b77964351dd27d055aa
shooter <shooter.hero@gmail.com>
2015-09-30 12:09:03 +0800

    add quicksort
    love zqw

---
+++ haskell/quick_sort.hs
@@ -0,0 +1,8 @@
+quicksort : (Ord a) => [a] -> [a]
+
+quicksort [] = []
+
+quicksort(x:xs) =
+  let smallSorted = quicksort [a | a <- xs, a <= x]
+      bigSorted = quicksort [a | a <- xs, a >= x ]
+  in smallSorted ++ [x] ++ bigSorted
