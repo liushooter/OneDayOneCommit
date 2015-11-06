7f9a8e5413e42ca9effe0f87a603d5c61e9b70c3
shooter <shooter.hero@gmail.com>
2015-11-07 01:20:54 +0800

    blog

--- python/Aztec/Aztec/urls.py
+++ python/Aztec/Aztec/urls.py
@@ -18,9 +18,7 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
 urlpatterns = [
-    url(r'^admin/', include(admin.site.urls)),
     url(r'^$', 'welcome.views.hello'),
-
-    # url(r'^blogs$/', 'blog.views.index'),
-    # url(r'^blogs/(\d+)$', 'blog.views.show')
+    url(r'^blog/', include('blog.urls')),
+    url(r'^admin/', include(admin.site.urls)),
 ]
--- python/Aztec/blog/views.py
+++ python/Aztec/blog/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Blog
 
 # Create your views here.
+
+def index(request):
+    blogs = Blog.objects.order_by('-created_at')[:5] # order by created_at desc limit 5
+    return HttpResponse(blogs)
