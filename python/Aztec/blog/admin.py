fd14d24f83e79bbdd993059be85fb081ea2fdb28
shooter <shooter.hero@gmail.com>
2015-11-07 00:38:47 +0800

    set column show name

--- python/Aztec/blog/admin.py
+++ python/Aztec/blog/admin.py
@@ -4,6 +4,10 @@ from .models import Blog
 # Register your models here.
 
 class BlogAdmin(admin.ModelAdmin):
-    fields = ['title', 'desc', 'url', 'is_publish']
+  # fields = ['title', 'desc', 'url', 'is_publish']
+  fieldsets = [
+    (None, {'fields': ['title', 'desc', 'url']}),
+    ('Published', {'fields': ['is_publish']}),
+  ]
 
 admin.site.register(Blog, BlogAdmin)
