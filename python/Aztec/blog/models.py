fa5787bbd694cd7d8cf19a3fec3e6b1953a5c97d
shooter <shooter.hero@gmail.com>
2015-11-06 14:06:29 +0800

    blog migrate

--- python/Aztec/blog/models.py
+++ python/Aztec/blog/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class Blog(models.Model):
+    # id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    title = models.CharField(max_length=200)
+    desc = models.TextField()
+    url = models.URLField()
+    votes = models.IntegerField(default=0)
