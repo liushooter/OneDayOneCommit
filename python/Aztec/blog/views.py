423eb2e195960c26bb0aa2be3226abb10f24406f
shooter <shooter.hero@gmail.com>
2015-11-07 01:29:33 +0800

    blog index page

--- python/Aztec/blog/views.py
+++ python/Aztec/blog/views.py
@@ -1,9 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import RequestContext, loader
 from .models import Blog
 
 # Create your views here.
 
 def index(request):
     blogs = Blog.objects.order_by('-created_at')[:5] # order by created_at desc limit 5
-    return HttpResponse(blogs)
+    template = loader.get_template('index.html')
+
+    context = RequestContext(request, {
+      'blog_list': blogs,
+    })
+
+    return HttpResponse(template.render(context))
