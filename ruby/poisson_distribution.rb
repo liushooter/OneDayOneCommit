7146527d1b82600da8559139a5c14d77a21e9e51
shooter <shooter.hero@gmail.com>
2015-12-13 02:25:56 +0800

    邹倩雯

---
+++ ruby/poisson_distribution.rb
@@ -0,0 +1,6 @@
+# 泊松分布
+
+# http://www.ruanyifeng.com/blog/2013/01/poisson_distribution.html
+# http://www.ruanyifeng.com/blog/2015/06/poisson-distribution.html
+# 泊松分布是单位时间内独立事件发生次数的概率分布，指数分布是独立事件的时间间隔的概率分布。
+# https://github.com/jonleighton/poisson
