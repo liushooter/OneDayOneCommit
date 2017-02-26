18975d9d0c823d4a0916b8f7135b162b29648dbb
shooter <shooter.hero@gmail.com>
2017-02-26 23:20:33 +0800

    SSE指令 nmmintrin

---
+++ c/nmmintrin.c
@@ -0,0 +1,14 @@
+//great_module.c
+#include <nmmintrin.h>
+
+#ifdef _MSC_VER
+    #define DLL_EXPORT __declspec( dllexport )
+#else
+    #define DLL_EXPORT
+#endif
+
+DLL_EXPORT int great_function(unsigned int n) {
+    return _mm_popcnt_u32(n);
+}
+
+// https://zhuanlan.zhihu.com/p/20152309
