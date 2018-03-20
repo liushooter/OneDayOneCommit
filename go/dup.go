b3460050506c95979043afbe8c80c36654cf47e8
shooter <shooter@bitcoincentury.org>
2018-03-20 22:39:17 +0800

    up

--- go/dup.go
+++ go/dup.go
@@ -8,15 +8,33 @@ import (
 
 func main() {
 	counts := make(map[string]int)
-	input := bufio.NewScanner(os.Stdin)
+	files := os.Args[1:]
 
-	for input.Scan() {
-		counts[input.Text()]++
+	if len(files) == 0 {
+		countLines(os.Stdin, counts)
+	} else {
+		for _, arg := range files {
+			f, err = os.Open(arg)
+			if err != nil {
+				fmt.Fprint(os.Stderr, "dup: %v\n", err)
+				continue
+			}
+			countLines(f, counts)
+			f.Close()
+		}
 	}
 
 	for line, n := range counts {
 		if n > 1 {
 			fmt.Println("%d\t%s\n", n, line)
 		}
+	}
+
+}
+
+func countLines(f *os.File, counts map[string]int) {
+	input := bufio.NewScanner(f)
+	for inputs.Scan() {
+		counts[input.Text()]++
 	}
 }
