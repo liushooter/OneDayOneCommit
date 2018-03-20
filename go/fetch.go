2fc7f7041af3fffeb8fe1c8573a1dac083c06385
shooter <shooter@bitcoincentury.org>
2018-03-21 00:45:42 +0800

    goroutine

--- go/fetch.go
+++ go/fetch.go
@@ -2,28 +2,46 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"io/ioutil"
 	"net/http"
 	"os"
+	"time"
 )
 
 func main() {
+	start := time.Now()
+	ch := make(chan string)
+
 	for _, url := range os.Args[1:] {
-		resp, err := http.Get(url)
-
-		if err != nil {
-			fmt.Fprintf(os.Stderr, "fetch: %v\n", err)
-			os.Exit(1)
-		}
-
-		b, err := ioutil.ReadAll(resp.Body)
-		resp.Body.Close()
-
-		if err != nil {
-			fmt.Fprintf(os.Stderr, "fetch: reading %s : %v\n", url, err)
-			os.Exit(1)
-		}
-
-		fmt.Printf("%s", b)
+		go fetch(url, ch) // 启动一个 goroutine
 	}
+
+	for range os.Args[1:] {
+		fmt.Println(<-ch) // 从ch通道接收
+	}
+
+	fmt.Printf("%.2fs  elapsed\n", time.Since(start).Seconds())
+}
+
+func fetch(url string, ch chan<- string) {
+	start := time.Now()
+	resp, err := http.Get(url)
+
+	if err != nil {
+		ch <- fmt.Sprint(err) // 发送到通道 ch
+		return
+	}
+
+	nbytes, err := io.Copy(ioutil.Discard, resp.Body)
+	resp.Body.Close() // don't leak resources
+
+	if err != nil {
+		ch <- fmt.Sprint("while reading %s: %v", url, err)
+		return
+	}
+
+	secs := time.Since(start).Seconds()
+	ch <- fmt.Sprintf("%.2f  %7d  %s", secs, nbytes, url)
+
 }
