440b841f7eda14fa64302b0cc3783f52b5214c05
shooter <shooter@bitcoincentury.org>
2018-06-09 01:25:53 +0800

    SPARQL Protocol and RDF Query Language

---
+++ ruby/sparql.rb
@@ -0,0 +1,27 @@
+#gem install sparql
+#http://www.rubydoc.info/github/ruby-rdf/sparql/frames
+
+require 'sparql/client'
+
+endpoint = "https://query.wikidata.org/sparql"
+sparql = <<'SPARQL'.chop
+SELECT ?person ?personLabel 
+WHERE 
+{
+  ?person wdt:P39 wd:Q268218.
+  ?person wdt:P27 wd:Q9683.
+  SERVICE wikibase:label { bd:serviceParam wikibase:language "zh". }
+}
+SPARQL
+
+client = SPARQL::Client.new(endpoint, :method => :get)
+rows = client.query(sparql)
+
+puts "Number of rows: #{rows.size}"
+for row in rows
+  for key,val in row do
+    print "#{key}: #{val}\t"
+  end
+  print "\n"
+end
+
